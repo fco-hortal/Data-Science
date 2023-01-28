80739d3b94a65772f9e91435c6e875d94a1baf56
fco-hortal <francisco.hortal@uc.cl>
2023-01-28 17:25:34 -0300

    Data Mining 2022-2

--- C2/parte_2/js/script.js
+++
@@ -1,9 +0,0 @@
-<img src="img/2000px-Lastfm_logo.png" width="150"/>
-
-<img src="img/10_canciones_mas_escuchadas.png" width="300"/>
-<img src="img/generos_mas_escuchados.png" width="300"/>
-<img src="img/canciones_escuchadas_por_mes.png" width="300"/>
-<img src="img/canciones_escuchadas_cada_dia_de_la_semana.png" width="300"/>
-<img src="img/canciones_escuchadas_por_hora_del_dia.png" width="300"/>
-        
-   
